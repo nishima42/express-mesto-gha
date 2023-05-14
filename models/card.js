d7a77a9805bbbbc39caea0be8387be9afae7ad39
Rustam Malikov <mrj@lowcost-mebel.ru>
2023-05-14 23:28:37 +0300

    ref: настроить валидацию ссылки

--- models/card.js
+++ models/card.js
@@ -10,6 +10,13 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator: (v) => {
+        const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+        return urlRegex.test(v);
+      },
+      message: 'Неверный формат ссылки на аватар',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
